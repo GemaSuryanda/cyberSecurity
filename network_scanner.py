cccf5cd38dba51a69730de510ac65863cddbcb1d
Gema Suryanda <gemagmsuryanda@gmail.com>
2023-11-20 08:22:51 +0700

    bisa gem

--- network_scanner.py
+++ network_scanner.py
@@ -1,7 +1,35 @@
+#!/usr/bin/env python
 import scapy.all as scapy
+# import argparse
+import optparse
+
+
+# def get_arguments():
+#     parser = optparse.OptionParser()
+#     parser.add_option("-t", "--target", dest="target", help="Target IP / IP range.")
+#     options, argument = parser.parse_args()
+#     return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered_list)
+    # clients_list = []
+    # for element in answered_list:
+    #     client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+    #     clients_list.append(client_dict)
+    # return clients_list
 
-scan("172.26.224.1/24")
+# def print_result(results_list):
+#     print("IP\t\t\tMAC Address\n-------------------------------------")
+#     for client in results_list:
+#         print(client["ip"] + "\t\t" + client["mac"])
+
+
+# options = get_arguments()
+# scan_result = scan(options.target)
+# print_result(scan_result)
+
+scan('172.26.229.1/24')
